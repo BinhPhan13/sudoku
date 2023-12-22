d7a1e8b73cd618674a9e1cf407742d850889be8a
Binh Phan <ptbinh2003@gmail.com>
2023-12-22 09:20:57 +0000

    add solver

---
+++ solver.py
@@ -0,0 +1,65 @@
+from board import Board
+import random
+from copy import copy
+
+class Solver:
+    def __init__(self,
+        board:Board, *,
+        max_solutions=2,
+    ):
+        self._board = board
+        self._holes = board.holes()
+
+        self._solutions = []
+        self._max_solutions = max_solutions
+
+        self._n = 0
+
+    def solve(self):
+        if self.num_solutions >= self._max_solutions:
+            return
+
+        if not self._holes:
+            self._solutions.append(copy(self._board))
+            return
+
+        self._n += 1
+        # choose the branch with least branches
+        self._holes.sort(key=lambda x: len(
+            self._board.possible_values(x)
+        ), reverse=True)
+        idx = self._holes.pop()
+        assert self._board[idx] == 0
+
+        values = list(self._board.possible_values(idx))
+        random.shuffle(values)
+        for v in values:
+            self._board[idx] = v
+            self.solve()
+
+        # backtrack
+        self._board[idx] = 0
+        self._holes.append(idx)
+
+    @property
+    def num_solutions(self):
+        return len(self._solutions)
+
+    @property
+    def solutions(self):
+        return [copy(b) for b in self._solutions]
+
+    @property
+    def num_trials(self):
+        return self._n
+
+if __name__ == '__main__':
+    from board import SETUP
+    b = Board(SETUP)
+    print(f'{len(b.holes())} holes')
+
+    s = Solver(b, max_solutions=2)
+    s.solve()
+
+    print(f'{s.num_trials=}')
+    print(f'{s.num_solutions=}')
